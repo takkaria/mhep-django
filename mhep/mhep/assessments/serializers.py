d25b10de0febf409946c4ed3c2d9e5ad164feb64
Paul Fawkesley <paul@paulfurley.com>
2019-08-21 11:35:07 +0100

    add assessments.id to serializers

--- mhep/mhep/assessments/serializers.py
+++ mhep/mhep/assessments/serializers.py
@@ -5,13 +5,22 @@ from mhep.assessments.models import Assessment
 class AssessmentMetadataSerializer(serializers.ModelSerializer):
     class Meta:
         model = Assessment
-        fields = ["name", "description", "openbem_version", "status", "created_at", "updated_at"]
+        fields = [
+            "id",
+            "name",
+            "description",
+            "openbem_version",
+            "status",
+            "created_at",
+            "updated_at",
+        ]
 
 
 class AssessmentFullSerializer(serializers.ModelSerializer):
     class Meta:
         model = Assessment
         fields = [
+            "id",
             "name",
             "description",
             "openbem_version",
