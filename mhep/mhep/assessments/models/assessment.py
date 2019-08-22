038f30f03cc0a98c6686f4466b13f2bdcccf7e7a
Paul Fawkesley <paul@paulfurley.com>
2019-08-22 15:04:26 +0100

    models.Assessment: allow `data` to be null
    it's valid for an assessment to be created without any data.

--- mhep/mhep/assessments/models/assessment.py
+++ mhep/mhep/assessments/models/assessment.py
@@ -28,7 +28,7 @@ class Assessment(models.Model):
         default='In progress',
     )
 
-    data = JSONField()
+    data = JSONField(null=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
