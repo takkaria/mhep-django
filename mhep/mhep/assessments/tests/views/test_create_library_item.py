1b972ff352bf36d7e27430d2c419d3527a73f9da
Ian Drysdale <idrysdale@gmail.com>
2019-09-05 17:36:48 +0100

    test you can't create dupe tags in library.data

--- mhep/mhep/assessments/tests/views/test_create_library_item.py
+++ mhep/mhep/assessments/tests/views/test_create_library_item.py
@@ -37,3 +37,30 @@ class TestCreateLibraryItem(APITestCase):
             )
 
         assert response.status_code == status.HTTP_204_NO_CONTENT
+
+    def test_create_library_item_fails_if_tag_already_exists(self):
+        library = Library.objects.create(
+            name="test library",
+            type="test type",
+            data={
+                "tag1": {"name": "foo"},
+            },
+        )
+
+        item_data = {
+            "tag": "tag1",
+            "item": {
+                "name": "bar",
+            }
+        }
+
+        response = self.client.post(
+            f"/api/v1/libraries/{library.id}/items/",
+            item_data,
+            format="json"
+        )
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data == {
+            "detail": f"tag `tag1` already exists in library {library.id}"
+        }
