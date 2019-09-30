9630d636be75325883c7b5f8ea02dd916b33eed5
Paul Fawkesley <paul@paulfurley.com>
2019-09-30 11:51:26 +0100

    add test for User.organisations

--- mhep/mhep/users/tests/test_models.py
+++ mhep/mhep/users/tests/test_models.py
@@ -6,3 +6,7 @@ pytestmark = pytest.mark.django_db
 
 def test_user_get_absolute_url(user: settings.AUTH_USER_MODEL):
     assert user.get_absolute_url() == f"/users/{user.username}/"
+
+
+def test_user_organisations(user_with_org: settings.AUTH_USER_MODEL):
+    assert user_with_org.organisations.all().count() == 1
