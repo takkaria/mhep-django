be433ebd57e9e1a31e02e1bff02b309b70c6304b
Paul Fawkesley <paul@paulfurley.com>
2019-10-07 12:37:37 +0100

    add `owner` to Library, add migration

--- mhep/mhep/assessments/models/library.py
+++ mhep/mhep/assessments/models/library.py
@@ -1,3 +1,6 @@
+import logging
+
+from django.conf import settings
 from django.db import models
 from django.contrib.postgres.fields import JSONField
 from mhep.assessments.validators import validate_dict
@@ -6,6 +9,13 @@ from mhep.assessments.validators import validate_dict
 class Library(models.Model):
     class Meta:
         verbose_name_plural = "libraries"
+
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=False, blank=False,
+        on_delete=models.PROTECT,
+        related_name="libraries",
+    )
 
     name = models.TextField()
     type = models.TextField()
